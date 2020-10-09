ad80c0d5048d5e2b273f92e80e407193dbf342bc
wangjianjun <jianjun_wang@andall.com>
2020-10-09 16:44:40 +0800

    源码分析子项目

---
+++ src/main/java/com/sp/sub/spprojsub/SpProjSubApplication.java
@@ -0,0 +1,13 @@
+package com.sp.sub.spprojsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpProjSubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpProjSubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/sub/spprojsub/SpProjSubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.sub.spprojsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpProjSubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
